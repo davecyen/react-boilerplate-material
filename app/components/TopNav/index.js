e3bd917e9b6dcc4ed20e185e9259cce95235dc0e
Dave Yen <davecyen@gmail.com>
2017-02-20 13:14:38 -0800

    [TopNav] Added list items into drawer

--- app/components/TopNav/index.js
+++ app/components/TopNav/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Drawer, MenuItem, Divider } from 'material-ui';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import {blue500, yellow600} from 'material-ui/styles/colors';
 
 export default class TopNav extends React.Component {
 
@@ -17,30 +21,74 @@ export default class TopNav extends React.Component {
     return (
       <div>
         <AppBar
-          title="Roomblocker"
-          onRightIconButtonTouchTap={this.handleToggle}
-          iconElementRight={
-            <IconButton>
-              <i className="material-icons md-24">menu</i>
-            </IconButton>
-          }
+          title="AppJoy"
+          onLeftIconButtonTouchTap={this.handleToggle}
           style={{
             backgroundColor: '#2d353f',
             boxShadow: 'none',
             fontFamily: 'Roboto, sans-serif',
           }}
-          showMenuIconButton={false}
         />
       <Drawer
         docked={false}
+        zDepth={0}
         open={this.state.open}
-        openSecondary={true}
         onRequestChange={(open) => this.setState({ open })}>
-          <MenuItem onTouchTap={this.handleClose}>
-            Home
-          </MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Requests</MenuItem>
+          <List>
+            <Subheader>App Name</Subheader>
+            <ListItem
+              primaryText="Leads"
+              leftAvatar={<Avatar icon={<i className="material-icons">star</i>} backgroundColor={yellow600} />}
+              rightIcon={
+                <i className="material-icons">settings</i>
+              }
+            />
+            <ListItem
+              primaryText="Contacts"
+              leftAvatar={<Avatar icon={<i className="material-icons">person</i>} backgroundColor={blue500} />}
+              rightIcon={
+                <i className="material-icons">settings</i>
+              }
+            />
+            <ListItem
+              primaryText="Accounts"
+              leftAvatar={<Avatar icon={<i className="material-icons">business</i>} backgroundColor={'purple'} />}
+              rightIcon={
+                <i className="material-icons">settings</i>
+              }
+            />
+            <ListItem
+              primaryText="Opportunities"
+              leftAvatar={<Avatar icon={<i className="material-icons">work</i>} backgroundColor={'green'} />}
+              rightIcon={
+                <i className="material-icons md-24">settings</i>
+              }
+            />
+            <ListItem
+              primaryText="Reports"
+              leftAvatar={<Avatar icon={<i className="material-icons">folder</i>} backgroundColor={''} />}
+              rightIcon={
+                <i className="material-icons md-24">settings</i>
+              }
+            />
+            <ListItem
+              primaryText="Dashboards"
+              leftAvatar={<Avatar icon={<i className="material-icons">folder</i>} backgroundColor={''} />}
+              rightIcon={
+                <i className="material-icons md-24">settings</i>
+              }
+            />
+          </List>
           <Divider />
+          <Subheader>Admin</Subheader>
+          <MenuItem onTouchTap={this.handleClose}>Users</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>Objects & Fields</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>Workflows & Approvals</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>Themes</MenuItem>
+          <Divider />
+          <Subheader>Account</Subheader>
+          <MenuItem onTouchTap={this.handleClose}>My Apps</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>Settings</MenuItem>
           <MenuItem onTouchTap={this.handleClose}>Sign out</MenuItem>
         </Drawer>
       </div>
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,6 +13,9 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import TopNav from 'components/TopNav';
 import messages from './messages';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import styled from 'styled-components';
 
 const Hero = styled.div`
@@ -25,6 +28,15 @@ const Hero = styled.div`
   width: 100%;
   display: inline-block;
   text-align: center;
+  padding: 15px;
+  margin-left: auto;
+  margin-right: auto
+`;
+
+const Container = styled.div`
+  margin-right: auto;
+  margin-left: auto;
+  padding: 15px;
 `;
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -33,13 +45,32 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
       <div>
         <TopNav />
         <Hero>
-          <h1
-            style={{
-              fontWeight: 100
-            }}>
-            Book hotels for groups and events
-          </h1>
+          <h1 style={{fontWeight: 100}}>App builder for business</h1>
+          <h2 style={{fontWeight: 100}}>Build, launch, and manage modern enterprise web applications without writing code.</h2>
+          <RaisedButton label="Get Started" primary={true} />
         </Hero>
+        <Container>
+          <Card>
+            <CardTitle title="Sales CRM" subtitle="Leads, Contacts, Accounts, Opportunities, Reports, Dashboards" />
+            <CardText>
+              Simple CRM for sales teams
+            </CardText>
+            <CardActions>
+              <FlatButton label="Preview" />
+              <FlatButton label="Create" />
+            </CardActions>
+          </Card>
+          <Card>
+            <CardTitle title="Customer Service" subtitle="Contacts, Accounts, Cases, Articles, Reports, Dashboards" />
+            <CardText>
+              Manage, escalate, and resolve customer service cases
+            </CardText>
+            <CardActions>
+              <FlatButton label="Preview" />
+              <FlatButton label="Create" />
+            </CardActions>
+          </Card>
+        </Container>
       </div>
     );
   }
